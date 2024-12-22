5c160c8db09b1e09d99d3ec32fef51e137422755
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-22 16:15:58 +0330

    Initial Commit

---
+++ PaperAppV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 02/10/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaperAppV2/PaperAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PaperAppV2App.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 02/10/1403.
+//
+
+import SwiftUI
+
+@main
+struct PaperAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
