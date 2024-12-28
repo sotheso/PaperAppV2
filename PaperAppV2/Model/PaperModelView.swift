327adb4345bfbfbc99f6804a2a5e820f43a49959
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-28 21:41:52 +0330

    .

---
+++ PaperAppV2/Model/PaperModelView.swift
@@ -0,0 +1,8 @@
+//
+//  PaperModelView.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 08/10/1403.
+//
+
+import Foundation
---
+++ PaperAppV2/View/AddPaperView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPaperView.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 08/10/1403.
+//
+
+import SwiftUI
+
+struct AddPaperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPaperView()
+}
