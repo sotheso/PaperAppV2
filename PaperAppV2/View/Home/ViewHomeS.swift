f8521494e3bc5b064bc6e71007bacb1db2795afc
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-29 13:04:10 +0330

    ViewHomeS1: First complex code

--- PaperAppV2/View/Home/ViewHomeS.swift
+++ PaperAppV2/View/Home/ViewHomeS.swift
@@ -21,10 +21,12 @@ var mackItems: [Item] = [
 ]
 
 struct ViewHomeS: View {
+    @State private var activePage: Int = 0
+    
     var body: some View {
         NavigationStack{
-            VStack{
-                ViewHomeS1{
+            VStack(spacing: 15){
+                ViewHomeS1(activeIndex: $activePage){
                     ForEach(mackItems){ item in
                         RoundedRectangle(cornerRadius: 15)
                             .fill(item.color.gradient)
@@ -32,6 +34,15 @@ struct ViewHomeS: View {
                     }
                 }
                 .frame(height: 220)
+                
+                HStack(spacing: 5) {
+                    ForEach(mackItems.indices, id: \.self) { index in
+                        Circle()
+                            .fill(activePage == index ? .primary : .secondary)
+                            .frame(width: 8, height: 8)
+                    }
+                }
+                .animation(.snappy, value: activePage)
             }
             .navigationTitle("Auto Scrool")
         }
--- PaperAppV2/View/Home/ViewHomeS1.swift
+++ PaperAppV2/View/Home/ViewHomeS1.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 struct ViewHomeS1<Content: View>: View {
+    @Binding var activeIndex: Int
+    
     @ViewBuilder var content: Content
     
     @State private var scrollPosition: Int?
+    @State private var offsetBasedScreen: Bool = false
+    @State private var isSettled: Bool = false
+    
     @State private var isScrolling: Bool = false
+    
+    // دست خر زیرش
+    @GestureState private var isHoldingScreen: Bool = false
+    @State private var timer = Timer.publish(every: autoSrollDuration, on: .main, in: .default).autoconnect()
     
     var body: some View {
         GeometryReader {
             let size = $0.size
             
-            ScrollView(.horizontal) {
-                HStack(spacing: 0) {
-                    Group(subviews: content) { collection in
-                        
+            Group(subviews: content) { collection in
+                ScrollView(.horizontal) {
+                    HStack(spacing: 0) {
                         if let lasItem = collection.last {
                             lasItem
                                 .frame(width: size.width, height: size.height)
                                 .id(-1)
-                                .onChange(of: isScrolling) { oldValue, newValue in
-                                    if !newValue && scrollPosition == -1 {
-                                        scrollPosition = collection.count - 1
-                                    }
-                                }
                         }
                         
                         ForEach(collection.indices, id: \.self) { index in
@@ -42,24 +45,87 @@ struct ViewHomeS1<Content: View>: View {
                             firstItem
                                 .frame(width: size.width, height: size.height)
                                 .id(collection.count)
-                                .onChange(of: isScrolling) { oldValue, newValue in
-                                    if !newValue && scrollPosition == collection.count {
-                                        scrollPosition = 0
-                                    }
-                                }
+                            
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollPosition(id: $scrollPosition)
+                .scrollTargetBehavior(.paging)
+                .scrollIndicators(.hidden)
+                
+                .onScrollPhaseChange{ oldPhase, newPhase in
+                    isScrolling = newPhase.isScrolling
+                    if  !isScrolling && scrollPosition == -1 {
+                        scrollPosition = collection.count - 1
+                    }
+                    
+                    if !isScrolling && scrollPosition == collection.count && !isHoldingScreen {
+                        scrollPosition = 0
+                    }
+                }
+                
+                .simultaneousGesture(DragGesture(minimumDistance: 0).updating($isHoldingScreen, body: {
+                    _ , state, _ in
+                    state = true
+                }))
+                
+                .onChange(of: isHoldingScreen, { oldValue , newValue in
+                    if newValue {
+                        timer.upstream.connect().cancel()
+                        
+                    } else {
+                        if isSettled && scrollPosition != offsetBasedScreen {
+                            scrollPosition = offsetBasedScreen
+                        }
+                        
+                        timer = Timer.publish(every: Self.autoSrollDuration, on: .main, in: .default).autoconnect()
+                    }
+                })
+                
+                .onReceive(timer) { _ in
+                    guard !isHoldingScreen && isScrolling else { return }
+                    
+                    let nextIndex = (scrollPosition ?? 0) + 1
+                    
+                    withAnimation(.snappy(duration: 0.5, extraBounce: 0)) {
+                        scrollPosition = (nextIndex == collection.count) ? 0 : nextIndex
+                        
+                    }
+                }
+                
+                .onChange(of: scrollPosition) { oldValue, newValue in
+                    if let newValue {
+                        if newValue == -1 {
+                            activeIndex = collection.count - 1
+                        } else if newValue == collection.count {
+                            activeIndex = 0
+                        } else {
+                            activeIndex = max(min(newValue, collection.count - 1), 0)
                         }
                     }
                 }
-                .scrollTargetLayout()
-            }
-            .scrollPosition(id: $scrollPosition)
-            .scrollTargetBehavior(.paging)
-            .scrollIndicators(.hidden)
-            .onScrollPhaseChange{ oldPhase, newPhase in
-                isScrolling = newPhase.isScrolling
+                .onScrollGeometryChange(for: CGFloat.self) {
+                    $0.contentOffset.x
+                } action: { oldValue ,newValue in
+                    
+                    isSettled = size.width > 0 ? (Int(newValue) % Int(size.width) == 0) : false
+                    let index = size.width > 0 ? Int((newValue / size.width).rounded() - 1) : 0
+                    offsetBasedScreen = index
+                    
+                    if isSettled  && (scrollPosition != index || index == collection.count) && !isHoldingScreen {
+                        scrollPosition = index == collection.count ? -0 : index
+                    }
+                }
             }
             .onAppear{ scrollPosition = 0 }
         }
+    }
+
+    
+    
+    static var autoSrollDuration: CGFloat {
+        return 1
     }
 }
 
