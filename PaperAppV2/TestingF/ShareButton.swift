9a5157bc7d92ceaa927edda204d5a895de40ca1d
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-22 12:46:48 +0330

    add Share Button (concept)

---
+++ PaperAppV2/TestingF/ShareButton.swift
@@ -0,0 +1,18 @@
+//
+//  ShareButton.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 03/11/1403.
+//
+
+import SwiftUI
+
+struct ShareButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareButton()
+}
