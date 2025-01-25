51466654fbdb7adb0d5fef6b89921f270c0380bd
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-01-25 16:39:54 +0330

    scrollTabTop testing (concept)

---
+++ PaperAppV2/TestingF/scrollTabTop/HomeTestView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTestView.swift
+//  PaperAppV2
+//
+//  Created by Sothesom on 06/11/1403.
+//
+
+import SwiftUI
+
+struct HomeTestView: View {
+    var columns = [GridItem(.adaptive(minimum: 159), spacing: 16)]
+    var body: some View {
+        ScrollView {
+            Text("Home")
+                .font(.largeTitle).bold()
+                .padding(.horizontal, 20)
+                .padding(.top, 17)
+                .id(1)
+            
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(0..<10) { number in
+                    sampleCard(image: "illustration", title: "SwiftUI for iOS 14", hours: "20 sections - 3 hours", colors: [Color(#colorLiteral(red: 1, green: 0.4509803922, blue: 0.4509803922, alpha: 1)), Color(#colorLiteral(red: 0.2862745098, green: 0.1176470588, blue: 0.7215686275, alpha: 1))], logo: "swift-logo")
+                }
+            }
+            .padding(20)
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+
+#Preview {
+    HomeTestView()
+}
